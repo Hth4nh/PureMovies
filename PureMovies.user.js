e1f6916181cfad73fb20cc04c6edefb03d64967f
Thành Hoàng Trần <hth4nh@gmail.com>
2025-01-21 03:24:19 +0700

    Add update & download URL

--- PureMovies.user.js
+++ PureMovies.user.js
@@ -1,29 +1,31 @@
 // ==UserScript==
-// @name         PureMovies
-// @namespace    Hth4nh
-// @version      1.2.1
-// @description  PureMovies là một user-script hoàn hảo dành cho những ai yêu thích trải nghiệm xem phim liền mạch, không bị gián đoạn bởi quảng cáo "lậu" trong phim. Hy vọng sẽ mang đến cảm giác thoải mái và tập trung, giúp bạn tận hưởng từng khoảnh khắc của bộ phim một cách trọn vẹn nhất.
-// @author       Thành Hoàng Trần (@hth4nh) và Team CukiPirate
-// @match        http*://kkphim.com/*
-// @match        http*://phim.nguonc.com/*
-// @match        http*://ophim17.cc/*
-// @match        https://player.phimapi.com/player/*
-// @match        http*://*.streamc.xyz/*
-// @match        http*://vip.opstream10.com/share/*
-// @match        http*://vip.opstream11.com/share/*
-// @match        http*://vip.opstream12.com/share/*
-// @match        http*://vip.opstream13.com/share/*
-// @match        http*://vip.opstream14.com/share/*
-// @match        http*://vip.opstream15.com/share/*
-// @match        http*://vip.opstream16.com/share/*
-// @match        http*://vip.opstream17.com/share/*
-// @match        http*://kkphim.vip/*
-// @match        http*://kkphim1.com/*
-// @match        http*://img.phimapi.com/*
-// @match        https://216.180.226.222/*
-// @icon         https://www.google.com/s2/favicons?sz=128&domain=kkphim.com
-// @run-at       document-body
-// @grant        none
+// @name             PureMovies
+// @namespace        Hth4nh
+// @version          1.2.1
+// @description      PureMovies là một user-script hoàn hảo dành cho những ai yêu thích trải nghiệm xem phim liền mạch, không bị gián đoạn bởi quảng cáo "lậu" trong phim. Hy vọng sẽ mang đến cảm giác thoải mái và tập trung, giúp bạn tận hưởng từng khoảnh khắc của bộ phim một cách trọn vẹn nhất.
+// @author           Thành Hoàng Trần (@hth4nh) và Team CukiPirate
+// @updateURL        https://github.com/Hth4nh/PureMovies/raw/refs/heads/main/PureMovies.user.js
+// @downloadURL      https://github.com/Hth4nh/PureMovies/raw/refs/heads/main/PureMovies.user.js
+// @match            http*://kkphim.com/*
+// @match            http*://phim.nguonc.com/*
+// @match            http*://ophim17.cc/*
+// @match            https://player.phimapi.com/player/*
+// @match            http*://*.streamc.xyz/*
+// @match            http*://vip.opstream10.com/share/*
+// @match            http*://vip.opstream11.com/share/*
+// @match            http*://vip.opstream12.com/share/*
+// @match            http*://vip.opstream13.com/share/*
+// @match            http*://vip.opstream14.com/share/*
+// @match            http*://vip.opstream15.com/share/*
+// @match            http*://vip.opstream16.com/share/*
+// @match            http*://vip.opstream17.com/share/*
+// @match            http*://kkphim.vip/*
+// @match            http*://kkphim1.com/*
+// @match            http*://img.phimapi.com/*
+// @match            https://216.180.226.222/*
+// @icon             https://www.google.com/s2/favicons?sz=128&domain=kkphim.com
+// @run-at           document-body
+// @grant            none
 // ==/UserScript==
 
 /* global Artplayer, Hls, url */
